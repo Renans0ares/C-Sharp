e05e516d0bcfc0324a0a682d9d4b05f16bfa0080
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-03-30 15:56:49 -0300

    Add files via upload

---
+++ Basic Exercises/MembrosEstaticos/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MembrosEstaticos
+{
+    class Calculadora
+    {
+        public static double PI = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return 2 * PI * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * PI * Math.Pow(r, 3);
+        }
+    }
+}
+
---
+++ Basic Exercises/MembrosEstaticos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Entre com o valor do raio:");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferenci: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.PI.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+
+        
+    }
+}
