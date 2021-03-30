a559ec312bf06e57fe0821e13bbd090123677fb3
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-03-30 17:28:56 -0300

    Add files via upload

---
+++ Basic Exercises/ExFixMembEstatico/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExFixMembEstatico
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double ValorReias(double cotacao, double qtdD)
+        {
+            double total = cotacao * qtdD;
+
+            return total + total * IOF / 100.0;
+        }
+    }
+}
---
+++ Basic Exercises/ExFixMembEstatico/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExFixMembEstatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double valorD = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.ValorReias(cotacao, valorD);
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
