759844848eca0db219e41fc96bfca206d1cd1335
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-04-09 09:36:40 -0300

    Add files via upload

---
+++ Basic Exercises/Auto Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("PS4", 3999.98, 2);
+
+            p.Nome = "PS4 PRO";
+
+            Console.WriteLine(p.Nome);
+
+
+        }
+    }
+}
