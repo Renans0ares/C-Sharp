d39de3f4642f56a8cb26b3ab719dc2e39dead10b
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-04-24 10:05:59 -0300

    Add files via upload

---
+++ Basic Exercises/ExemploStructs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExemploStructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p); //(10, 20)
+
+            p = new Point();
+            Console.WriteLine(p); //(0, 0)
+        }
+    }
+}
