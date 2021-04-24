fe5b4b6add9373158d85cd28cbee825d9a280f31
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-04-24 10:35:46 -0300

    Add files via upload

---
+++ Basic Exercises/Nullable/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nullable<double> x = null;
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault()); //0
+            Console.WriteLine(y.GetValueOrDefault()); //10
+
+            Console.WriteLine(x.HasValue); //False
+            Console.WriteLine(y.HasValue); //True
+
+            //Console.WriteLine(x.Value); Não imprime simplesmente
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X é NULL");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y é NULL");
+            }
+
+
+            double? a = null;
+            double b = a ?? 5;
+
+            Console.WriteLine(b);
+
+
+            
+        }
+    }
+}
