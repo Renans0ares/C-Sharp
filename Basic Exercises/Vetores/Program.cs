70b827d2a1a5cbbf6c9b31f62b390deba192c71b
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-04-24 11:07:07 -0300

    Add files via upload

---
+++ Basic Exercises/Vetores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n]; /*Declaração*/
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            foreach (double element in vect)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
