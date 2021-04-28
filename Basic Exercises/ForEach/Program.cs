87f96e85f55a19e791fa670e190bc9be2df1c9a2
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-04-28 17:09:21 -0300

    Add files via upload

---
+++ Basic Exercises/ForEach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = new string[] { "Renan", "Carlos", "Giovanni" };
+
+            /*for(int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }*/
+
+            int i = 1;
+            foreach(string cont in vetor)
+            {
+                Console.WriteLine($"#{i} Nome = {cont}");
+                i++;
+            }
+        }
+    }
+}
