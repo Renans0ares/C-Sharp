ebb9800c4b5249accb9ca5538e9d23b828cd9576
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-04-28 16:57:21 -0300

    Add files via upload

---
+++ Basic Exercises/ModificadoresParametros/ParamRefOut/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParamRefOut
+{
+    class Calculadora
+    {
+        public static void Triplo(ref int x) //Com a palavra ref é possivle realizar a mudança não no escopo da função,
+                                            //Mas na referencia do A
+        {
+            x = x * 3;
+        }
+
+        public static void Dobro(int origem, out int resultado)
+        {
+            resultado = origem * 2;
+        }
+    }
+}
---
+++ Basic Exercises/ModificadoresParametros/ParamRefOut/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ParamRefOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A tem que ser inciado por conta do ref
+            int a = 10;
+            Calculadora.Triplo(ref a);
+            Console.WriteLine(a);
+
+            int b = 10;
+            int dobro;
+
+            Calculadora.Dobro(b, out dobro); //Será criado do A e saírá o resultado no Dobro
+            Console.WriteLine(dobro);
+        }
+    }
+}
