271cc0f930b7713ab65e408a97daaa3523259360
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-04-28 16:45:20 -0300

    Add files via upload

---
+++ Basic Exercises/ModificadoresParametros/ModifParam/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModifParam
+{
+    class Calculadora
+    {
+        public static int Soma(params int[] num)
+        {
+            int soma = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                soma += num[i];
+            }
+            return soma;
+        }
+    }
+}
---
+++ Basic Exercises/ModificadoresParametros/ModifParam/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ModifParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar os vetores de uma forma mais compacta
+            //int n1 = Calculadora.Soma(new int[] { 10, 25, 14 }); dá para ficar melhor
+
+            //Utilizando Params
+            int n1 = Calculadora.Soma(10, 25, 14);
+
+            Console.WriteLine(n1);
+        }
+    }
+}
