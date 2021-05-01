c587324174d50c2305362cb49d2fdfe2e4ea6d04
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-05-01 13:56:47 -0300

    Add files via upload

---
+++ Basic Exercises/Matriz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arranjo bidmencional 
+            double[,] matriz = new double[2, 3];
+
+            Console.WriteLine(matriz.Length);
+
+            Console.WriteLine(matriz.Rank);
+
+            Console.WriteLine(matriz.GetLength(0));
+
+            Console.WriteLine(matriz.GetLength(1));
+
+        }
+    }
+}
