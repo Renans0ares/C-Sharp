15e7a579992052f051d77833a7634e20018a6b17
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-05-10 15:27:47 -0300

    Add files via upload

---
+++ Basic Exercises/Enum/Course/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment= 0, //O valor por padrão será 0 se não for deifinido.
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+
+    }
+}
