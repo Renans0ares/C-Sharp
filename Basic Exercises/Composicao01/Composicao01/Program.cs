5ee3e2b4cacbf13324ae1cf3889d88929422baf7
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-05-11 11:29:25 -0300

    Update Program.cs

--- Basic Exercises/Composicao01/Composicao01/Program.cs
+++ Basic Exercises/Composicao01/Composicao01/Program.cs
@@ -1,4 +1,4 @@
-﻿using Composicao01.Entities.Enums;
+using Composicao01.Entities.Enums;
 using System.Globalization;
 using System;
 using Composicao01.Entities;
@@ -29,12 +29,32 @@ namespace Composicao01
             Console.Write("How many contracts to this worker? ");
             int n = int.Parse(Console.ReadLine());
 
-            for(int i = 1; i < n; i++)
+            for(int i = 1; i <= n; i++)
             {
-                Console.WriteLine("Enter #{i} contract data: ");
+                Console.WriteLine($"Enter #{i} contract data: ");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Duration: ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+
+                worker.AddContract(contract);
 
             }
 
+            Console.WriteLine();
+            Console.Write("Enter month and year: ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Departament.Name);
+            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month));
         }
     }
 }
