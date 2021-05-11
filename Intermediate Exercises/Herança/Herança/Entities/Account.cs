fa4f19db23ef94b8fb27832d00ef02256609d23d
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-05-11 14:20:50 -0300

    Add files via upload

---
+++ Intermediate Exercises/Herança/Herança/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balane { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balane)
+        {
+            Number = number;
+            Holder = holder;
+            Balane = balane;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balane -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balane += amount;
+        }
+    }
+}
