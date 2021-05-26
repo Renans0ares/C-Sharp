f1e28c6cb06187c28f3a39b4b9610e409ac65bb0
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-05-26 13:46:14 -0300

    Add files via upload

---
+++ Intermediate Exercises/ClassesAbstratas/ClassesAbstratas/Entities/SavingAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAbstratas.Entities
+{
+    class SavingsAccounts : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccounts()
+        {
+        }
+
+        public SavingsAccounts(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount) //Reesrever com outro comportamento
+        {
+            //Balance -= amount;
+            //Outro exemplo
+            base.Withdraw(amount);//Operacao de saque na superclasse
+            Balance -= 2.0;
+
+        }
+    }
+}
---
+++ Intermediate Exercises/ClassesAbstratas/ClassesAbstratas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassesAbstratas.Entities;
+using System.Globalization;
+
+namespace ClassesAbstratas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingsAccounts(1001, "Renan", 500.0, 0.01));
+            list.Add(new BusinessAccount(1002, "Carlos", 500.0,400.0));
+            list.Add(new SavingsAccounts(1003, "Giovanni", 500.0, 0.01));
+            list.Add(new BusinessAccount(1004, "Yan", 500.0, 500.0));
+
+            double sum = 0.0;
+
+            foreach(Account count in list)
+            {
+                sum += count.Balance;
+            }
+
+            Console.WriteLine("Total balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach(Account count in list)
+            {
+                count.Withdraw(10.0);
+            }
+
+            foreach(Account count in list)
+            {
+                Console.WriteLine("Update balance for account "
+                    + count.Number
+                    + ": "
+                    + count.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
