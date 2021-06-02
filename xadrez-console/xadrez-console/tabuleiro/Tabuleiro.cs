89ab100700037b6dda2c6073c4d2f903404d7ba0
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-06-02 14:22:47 -0300

    Add files via upload

--- xadrez-console/xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/xadrez-console/tabuleiro/Tabuleiro.cs
@@ -28,6 +28,7 @@
             validarPosicao(pos);
             return peca(pos) != null;
         }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
             if (existePeca(pos))
@@ -38,16 +39,26 @@
             p.posicao = pos;
         }
 
+        public Peca retirarPeca(Posicao pos)
+        {
+            if(peca(pos) == null)
+            {
+                return null;
+            }
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux;
+        }
+
         public bool posicaoValida(Posicao pos)
         {
             if(pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
             {
                 return false;
             }
-            else
-            {
-                return true;
-            }
+
+            return true;
         }
 
         public void validarPosicao(Posicao pos)
