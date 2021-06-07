fed30c7a21a056a8ca1d17858597327a0771b632
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-06-07 10:58:30 -0300

    Add files via upload

---
+++ Intermediate Exercises/TrabalhandoArquivos/TrabalhandoArquivos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace TrabalhandoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                        //Colocar caminho
+            string sourcePath = @"";
+            string targetPath = @"";
+
+            try
+            {
+                //FileInfo fileinfo = new FileInfo(sourcePath);
+                //fileinfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An erro occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
