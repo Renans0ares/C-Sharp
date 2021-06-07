a81368a76493b0e6df05e9520c253e9c4534c3ee
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-06-07 11:17:02 -0300

    Add files via upload

---
+++ Intermediate Exercises/FileStreamandStremReader/FileStreamandStremReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamandStremReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\renan.sousa\Documents\File.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);//Abrir
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accured");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
