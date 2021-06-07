32ed38e7a6f681218221cadac370ad27ecb25780
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-06-07 11:19:05 -0300

    Add files via upload

---
+++ UsandoUsing/FileStreamandStremReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamandStremReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\renan.sousa\Documents\File.txt";
+            
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using(StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
